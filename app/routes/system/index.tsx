34edef6baa4711536538b93017562b138dd9aa73
cp <sockfasteat@gmail.com>
2021-12-25 15:01:17 -0800

    add prisma

--- app/routes/system/index.tsx
+++ app/routes/system/index.tsx
@@ -1,6 +1,31 @@
 import { Outlet } from 'remix';
 
+import { useLoaderData, LoaderFunction } from 'remix';
+import { db } from '~/database/db.server';
+
+import { styled } from '~/styles/stitches.config';
+
+export interface Image {
+  url: string;
+  caption: string;
+}
+
+export type LoaderData = {
+  images: Image[];
+};
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    images: await db.image.findMany(),
+  };
+  return data;
+};
+
 export default function SystemIndexRoute() {
+  const { images } = useLoaderData();
+
+  console.log(images);
+
   return (
     <div>
       lol
