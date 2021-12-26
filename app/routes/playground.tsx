bb61c5cfbf033d537b986bc9e23bc72270f309a0
cp <sockfasteat@gmail.com>
2021-12-26 12:48:04 -0800

    nice

--- app/routes/playground.tsx
+++ app/routes/playground.tsx
@@ -1,7 +1,8 @@
 import { purple, sky } from '@radix-ui/colors';
 import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 import { Flex } from '~/components/Flex';
-import { styled, keyframes } from '~/styles/stitches.config';
+import { NoiseBackground } from '~/components/Noise';
+import { styled } from '~/styles/stitches.config';
 import { GG_BRIDGE } from './system/components/memory-scroller';
 
 const Image = styled('img', {
@@ -14,6 +15,7 @@ const Image = styled('img', {
   0 16px 16px hsl(0deg 0% 0% / 0.075)`,
   zIndex: -1,
   position: 'relative',
+  cursor: 'pointer',
 });
 
 interface ParallaxProps {
@@ -28,58 +30,33 @@ interface ParallaxProps {
   /* pass in the same value twice for a static offset */
 }
 const parallaxConfig: ParallaxProps[] = [
-  { y: ['400px', '0px'], x: ['0px', '0px'] },
-  { y: ['100px', '100px'], x: ['330px', '330px'] },
-  { y: ['400px', '0px'], x: ['-200px', '-200px'] },
-  { y: ['100px', '100px'], x: ['330px', '330px'] },
-  { y: ['400px', '0px'], x: ['-200px', '-200px'] },
-  { y: ['100px', '100px'], x: ['330px', '330px'] },
-  { y: ['400px', '0px'], x: ['-200px', '-200px'] },
-  { y: ['100px', '100px'], x: ['330px', '330px'] },
-  { y: ['400px', '0px'], x: ['-200px', '-200px'] },
-  { y: ['100px', '100px'], x: ['330px', '330px'] },
-  { y: ['400px', '0px'], x: ['-200px', '-200px'] },
-  { y: ['100px', '100px'], x: ['330px', '330px'] },
-  { y: ['400px', '0px'], x: ['-200px', '-200px'] },
-  { y: ['100px', '100px'], x: ['330px', '330px'] },
-  { y: ['400px', '0px'], x: ['-200px', '-200px'] },
-  { y: ['100px', '100px'], x: ['330px', '330px'] },
-  { y: ['400px', '0px'], x: ['-200px', '-200px'] },
-  { y: ['100px', '100px'], x: ['330px', '330px'] },
-  { y: ['400px', '0px'], x: ['-200px', '-200px'] },
-  { y: ['100px', '100px'], x: ['330px', '330px'] },
+  { y: ['400px', '0px'], x: ['-350px', '-350px'] }, // 1
+  { y: ['0px', '0px'], x: ['300px', '300px'] }, // 2
+  { y: ['200px', '0px'], x: ['-200px', '-200px'] }, // 3
+  { y: ['100px', '100px'], x: ['400px', '400px'] }, // 4
+  { y: ['400px', '200px'], x: ['50px', '50px'] }, // 5
+  { y: ['100px', '-800px'], x: ['-400px', '-400px'] }, // 6
+  { y: ['0px', '0px'], x: ['-300px', '-300px'] }, // 7
+  { y: ['100px', '-800px'], x: ['400px', '400px'] }, // 8
+  { y: ['100px', '-300px'], x: ['230px', '230px'] }, // 9
+  { y: ['150px', '-500px'], x: ['-400px', '-400px'] }, // 10
+  { y: ['0px', '0px'], x: ['-200px', '-200px'] }, // 11
+  { y: ['100px', '-800px'], x: ['330px', '330px'] }, // 12
+  { y: ['100px', '-500px'], x: ['-300px', '-300px'] }, // 13
+  { y: ['400px', '0px'], x: ['-350px', '-350px'] }, // 1
+  { y: ['0px', '0px'], x: ['300px', '300px'] }, // 2
+  { y: ['200px', '0px'], x: ['-200px', '-200px'] }, // 3
+  { y: ['100px', '100px'], x: ['400px', '400px'] }, // 4
+  { y: ['400px', '200px'], x: ['50px', '50px'] }, // 5
+  { y: ['100px', '-800px'], x: ['-400px', '-400px'] }, // 6
+  { y: ['0px', '0px'], x: ['-300px', '-300px'] }, // 7
+  { y: ['100px', '-800px'], x: ['400px', '400px'] }, // 8
+  { y: ['100px', '-300px'], x: ['230px', '230px'] }, // 9
+  { y: ['150px', '-500px'], x: ['-400px', '-400px'] }, // 10
+  { y: ['0px', '0px'], x: ['-200px', '-200px'] }, // 11
+  { y: ['100px', '-800px'], x: ['330px', '330px'] }, // 12
+  { y: ['100px', '-500px'], x: ['-300px', '-300px'] }, // 13
 ];
-
-const noiseAnimation = keyframes({
-  '0%': { transform: 'translate(0, 0)' },
-  '10%': { transform: 'translate(-5%, -5%)' },
-  '20%': { transform: 'translate(-10%, 5%)' },
-  '30%': { transform: 'translate(5%, -10%)' },
-  '40%': { transform: 'translate(-5%, 15%)' },
-  '50%': { transform: 'translate(-10%,5%)' },
-  '60%': { transform: 'translate(15%,0)' },
-  '70%': { transform: 'translate(0,10%)' },
-  '80%': { transform: 'translate(-15%,0)' },
-  '90%': { transform: 'translate(10%, 5%)' },
-  '100%': { transform: 'translate(5%, 0)' },
-});
-
-export const NoiseBackground = styled('div', {
-  position: 'fixed',
-  top: '-50%',
-  left: '-50%',
-  right: '-50%',
-  bottom: '-50%',
-  width: '200%',
-  height: '200vh',
-  background:
-    "transparent url('http://assets.iceable.com/img/noise-transparent.png') repeat 0 0",
-  backgroundRepeat: 'repeat',
-  animation: `${noiseAnimation} 1s infinite`,
-  opacity: '0.7',
-  color: 'red',
-  zIndex: 0,
-});
 
 const string = `${sky.sky5}, white, ${purple.purple5}`;
 
@@ -92,77 +69,27 @@ const PlaygroundRoute = () => {
         <Flex
           layout={'centerColumn'}
           css={{
-            height: 'fit-content',
+            height: 10000,
             width: '100%',
-            background: `linear-gradient(165deg, ${string}, ${string}, ${string})`,
+            background: `linear-gradient(165deg, ${string}, ${string}, ${string}, ${string}, ${string})`,
             // background: 'white',
             zIndex: 0,
             backgroundBlendMode: 'screen',
+            position: 'relative',
           }}
         >
+          <NoiseBackground />
           {parallaxConfig.map((config, i) => {
             return (
               <Parallax x={config.x} y={config.y}>
-                <Image id={`parallax-image-${i}`} width={400} src={GG_BRIDGE} />
+                <Image
+                  id={`parallax-image-${i + 1}`}
+                  width={400}
+                  src={GG_BRIDGE}
+                />
               </Parallax>
             );
           })}
-          <NoiseBackground />
-
-          {/* <Parallax y={['100px', '100px']} x={[70, 70]}>
-            <Image width={400} src={GG_BRIDGE}></Image>
-          </Parallax> */}
-
-          {/* <Parallax y={['100px', '-200px']} x={['-300px', '-300px']}>
-            <Image id="image-3" width={400} src={GG_BRIDGE}></Image>
-          </Parallax>
-
-          <Parallax y={['200px', '-400px']} x={['150px', '150px']}>
-            <Image id="image-4" src={GG_BRIDGE}></Image>
-          </Parallax>
-
-          <Parallax y={['300px', '300px']} x={['400px', '400px']}>
-            <Image id="image-5" width={400} src={GG_BRIDGE}></Image>
-          </Parallax>
-
-          <Parallax y={['100px', '-900px']} x={[-70, -70]}>
-            <Image id="image-6" width={400} src={GG_BRIDGE}></Image>
-          </Parallax>
-
-          <Parallax y={['200px', '-800px']} x={['-200px', '-200px']}>
-            <Image id="image-7" src={GG_BRIDGE}></Image>
-          </Parallax>
-
-          <Parallax y={['100px', '100px']} x={[70, 70]}>
-            <Image width={400} src={GG_BRIDGE}></Image>
-          </Parallax>
-
-          <Parallax y={['100px', '-200px']} x={[-70, -70]}>
-            <Image width={400} src={GG_BRIDGE}></Image>
-          </Parallax>
-          <Parallax y={['200px', '0px']} x={[0, 0]}>
-            <Image src={GG_BRIDGE}></Image>
-          </Parallax>
-
-          <Parallax y={['100px', '100px']} x={[70, 70]}>
-            <Image width={400} src={GG_BRIDGE}></Image>
-          </Parallax>
-
-          <Parallax y={['100px', '-200px']} x={[-70, -70]}>
-            <Image width={400} src={GG_BRIDGE}></Image>
-          </Parallax>
-
-          <Parallax y={['200px', '0px']} x={[0, 0]}>
-            <Image src={GG_BRIDGE}></Image>
-          </Parallax>
-
-          <Parallax y={['100px', '100px']} x={[70, 70]}>
-            <Image width={400} src={GG_BRIDGE}></Image>
-          </Parallax>
-
-          <Parallax y={['100px', '-200px']} x={[-70, -70]}>
-            <Image width={400} src={GG_BRIDGE}></Image>
-          </Parallax> */}
         </Flex>
       </ParallaxProvider>
     </>
