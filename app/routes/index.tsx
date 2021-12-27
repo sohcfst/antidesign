6c83af30d93e32630840690b62635fafc67db449
cp <sockfasteat@gmail.com>
2021-12-27 13:42:31 -0800

    add line-trhough

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,10 +2,17 @@ import { Flex } from '~/components/Flex';
 import { NoiseBackground } from '~/components/Noise';
 import { LinkTray } from '~/components/Sidebar/Sidebar';
 import { H1 } from '~/components/Typography/Header';
+import { Paragraph } from '~/components/Typography/Text';
 import { globalStyles } from '~/styles/stitches.config';
 
 export default function Index() {
   globalStyles();
+  const lol = [
+    { title: 'ARTIFACT_0001', date: 'AUGUST 8, 1906' },
+    { title: 'ARTIFACT_0023', date: 'AUGUST 8, 1906' },
+    { title: 'ARTIFACT_0112', date: 'AUGUST 8, 1906' },
+    { title: 'ARTIFACT_0014', date: 'AUGUST 8, 1906' },
+  ];
 
   return (
     <Flex css={{ py: 40 }} layout={'centerColumn'}>
@@ -13,13 +20,11 @@ export default function Index() {
       <Flex
         layout={'centerColumn'}
         css={{
-          border: '1px solid white',
           width: 1192,
           height: 98,
           br: 8,
           padding: 16,
           z: 1,
-          background: '$stitchesGrey',
         }}
       >
         <H1
@@ -27,6 +32,8 @@ export default function Index() {
             fontSize: 48,
             color: 'black',
             textShadow: '-2px 0 white, 0 2px white, 2px 0 white, 0 -2px white',
+            textDecoration: 'line-through',
+            cursor: 'pointer',
           }}
         >
           ANTIDESIGN
@@ -48,20 +55,44 @@ export default function Index() {
             height: '100%',
             border: '1px solid white',
             br: 8,
+            boxShadow: `0 1px 1px hsl(0deg 100% 100% / 0.075),
+            0 2px 2px hsl(0deg 100% 100% / 0.075),
+            0 4px 4px hsl(0deg 100% 100% / 0.075),
+            0 8px 8px hsl(0deg 100% 100% / 0.075),
+            0 16px 16px hsl(0deg 100% 100% / 0.075)`,
           }}
         >
           <LinkTray />
         </Flex>
         <Flex
+          layout="centerColumn"
           css={{
             height: '100%',
-            border: '1px solid white',
+
             br: 8,
             width: '100%',
-            padding: 16,
+            px: 24,
+            fontWeight: 'bold',
+            gap: 16,
           }}
         >
-          i be wearing carhartt with no works in progress
+          {lol.map((lol) => {
+            return (
+              <Flex
+                css={{
+                  height: '100%',
+                  border: '1px solid white',
+                  br: 8,
+                  width: '100%',
+                  padding: 24,
+                  flexDirection: 'column',
+                }}
+              >
+                <Paragraph css={{ fontSize: 24 }}>{lol.title}</Paragraph>
+                <Paragraph>{lol.date}</Paragraph>
+              </Flex>
+            );
+          })}
         </Flex>
       </Flex>
     </Flex>
