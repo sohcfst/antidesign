76149bab8b17a3f0812a3543f07204d70b03f2f0
cp <sockfasteat@gmail.com>
2022-01-08 14:29:01 -0800

    use styledheader

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,12 @@
 import { Section } from '~/rest/components/Section';
-import { AntidesignHeader } from '~/rest/pages/index/AntidesignHeader';
+import { StyledHeader } from '~/rest/components/Typography/Header/Header.styled';
 
 import { ContentContainer } from '~/rest/pages/index/Content/ContentContainer';
 
 export default function IndexRoute() {
   return (
     <Section css={{ flexDirection: 'column', p: 24 }}>
-      <AntidesignHeader />
+      <StyledHeader>A N T I D E S I G N</StyledHeader>
       <ContentContainer />
     </Section>
   );
--- app/routes/playground.tsx
+++ app/routes/playground.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'remix';
 
 import { Section } from '~/rest/components/Section';
-import { PlaygroundHeader } from '~/rest/pages/playground/PlaygroundHeader';
+import { StyledHeader } from '~/rest/components/Typography/Header/Header.styled';
 
 export default function PlaygroundRoute() {
   return (
     <Section css={{ flexDirection: 'column', p: 24, alignItems: 'center' }}>
-      <PlaygroundHeader />
+      <StyledHeader>P L A Y G R O U N D</StyledHeader>
       <Outlet />
     </Section>
   );
